9ec7a17ded904d02b1dc89d3ad8049e923f649aa
ESSO <islamdiab967@gmail.com>
2023-02-11 08:39:40 +0200

    import home component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import React from 'react'
+import { Route, Routes } from "react-router-dom";
+import Home from './components/Home/Home';
+
 function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
