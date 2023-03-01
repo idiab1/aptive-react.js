1d35f1d6d69245baebc1d2908be91f1f70d6dedf
ESSO <islamdiab967@gmail.com>
2023-03-01 09:35:21 +0200

    create a new component for asked sections

---
+++ src/components/Asked/Asked.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import './Asked.css'
+
+const Asked = () => {
+  return (
+    <div className='asked-section'>
+        <div className='container'>
+            <div className='content'>
+                <div className='col-1'>
+                    <h2>Frequently asked questions</h2>
+                    <p>Can't find the answer you're looking for?</p>
+                    <p>Reach out to our <span className='primary-color'>customer support</span> team.</p>
+                </div>
+                <div className='col-2'>
+                    <ul>
+                        <li>
+                            <h4>How do you make holy water?</h4>
+                            <p>You biol the hell out of it. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
+                        </li>
+                        <li>
+                            <h4>How do you make holy water?</h4>
+                            <p>You biol the hell out of it. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
+                        </li>
+                        <li>
+                            <h4>How do you make holy water?</h4>
+                            <p>You biol the hell out of it. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
+                        </li>
+                        <li>
+                            <h4>How do you make holy water?</h4>
+                            <p>You biol the hell out of it. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Asked
